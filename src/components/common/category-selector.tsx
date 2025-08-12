5224c27ecc746face9f815a50027c0313dc1e69b
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2025-08-12 17:34:46 -0300

    feat: add category products page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { categoryTable } from "@/db/schema";
 
 import { Button } from "../ui/button";
@@ -8,18 +10,17 @@ interface CategorySelectorProps {
 
 const CategorySelector = ({ categories }: CategorySelectorProps) => {
   return (
-    <div className="rouded-3xl p-6 bg-[#F4EFFF]">
+    <div className="rounded-3xl bg-[#F4EFFF] p-6">
       <div className="grid grid-cols-2 gap-3">
         {categories.map((category) => (
           <Button
             key={category.id}
             variant="ghost"
-            className="bg-white rounded-full fount-semibold text-xs"
+            className="rounded-full bg-white text-xs font-semibold"
           >
-            {category.name}
+            <Link href={`/category/${category.slug}`}>{category.name}</Link>
           </Button>
         ))}
-        ;
       </div>
     </div>
   );
