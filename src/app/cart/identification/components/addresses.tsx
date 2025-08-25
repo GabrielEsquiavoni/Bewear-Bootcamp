1050b7bab5ec785def01fd3b63ff37ecadc35886
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2025-08-25 15:48:48 -0300

    feat: add cart summary component

--- src/app/cart/identification/components/addresses.tsx
+++ src/app/cart/identification/components/addresses.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { PatternFormat } from "react-number-format";
 import { toast } from "sonner";
@@ -24,7 +24,6 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { shippingAddressTable } from "@/db/schema";
 import { useCreateShippingAddress } from "@/hooks/mutations/use-create-shipping-address";
 import { useUpdateCartShippingAddress } from "@/hooks/mutations/use-update-cart-shipping-address";
-import { useCart } from "@/hooks/queries/use-cart";
 import { useUserAddresses } from "@/hooks/queries/use-user-addresses";
 
 const formSchema = z.object({
@@ -61,14 +60,6 @@ const Addresses = ({
   const { data: addresses, isLoading } = useUserAddresses({
     initialData: shippingAddresses,
   });
-
-  const { data: cart } = useCart();
-
-  useEffect(() => {
-    if (cart?.shippingAddress?.id) {
-      setSelectedAddress(cart.shippingAddressId);
-    }
-  }, [cart]);
 
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
